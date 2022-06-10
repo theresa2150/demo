a07a8cb92d8b4b84387cf81d24d4cab3f42d4a42
Theresa <theresa2150@hotmail.com>
2022-06-10 15:25:50 +0800

    first commit

---
+++ src/main/java/com/example/demo/bean/Seaball.java
@@ -0,0 +1,14 @@
+package com.example.demo.bean;
+
+public class Seaball {
+  String num;
+  String name;
+
+  public String getNum() {
+    return num;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
---
+++ src/main/java/com/example/demo/controller/Read.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.bean.Seaball;
+
+@RestController
+public class Read {
+
+  @GetMapping("/readSeaballs/{index}")
+  public @ResponseBody String readSeaballs(@PathVariable String index) {
+    return "There are " + index + " seaballs";
+  }
+
+  @PostMapping("/showSeaballs")
+  public @ResponseBody String showSeaballs(@RequestBody Seaball seaball) {
+    return "The " + seaball.getNum() + " seaball is " + seaball.getName();
+  }
+
+}
